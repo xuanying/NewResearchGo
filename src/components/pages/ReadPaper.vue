0f8daa49ef68f317835602cfe849847ce339411a
xuanying <983039901@qq.com>
2018-09-26 10:30:15 +0800

    readPaper complete articleDetail ing

--- src/components/pages/ReadPaper.vue
+++ src/components/pages/ReadPaper.vue
@@ -10,23 +10,28 @@
             <input id="list_filter" name="filter" class="input" type="text">
             <input type="button" value="">
           </div>
-          <span id="change_word">换一批</span>
+          <span id="change_word" @click="chnagePaper()">换一批</span>
         </section>
         <section class="result_box">
-          <div class="re_block" v-for="(item,index) in paperInfo" :key="index">
+          <div class="re_block" v-for="(item,index) in paperInfo" :key="index" v-show="changepage(index)">
             <span class="se_year">{{item.Paper_Year}}</span>
             <h1><a>{{item.Paper_Title}}</a></h1>
-            <p class="se_author" v-for="(Author,index) in item.Paper_Author" :key="index">
-                <a >{{Author.Author_Name + '(' + Author.Author_Level + ')'}}</a>,
+            <p class="se_author" v-for="(Author,index) in item.Paper_Author" :key="index+1000">
+              <a>{{Author.Author_Name + '(' + Author.Author_Level + ')'}}</a>,
             </p>
-            <p class="se_jour"><a href="">Proceedings of International Conference(A)</a></p>
-            <p class="se_abs"><span class="se_type">摘要</span>A key problem in learning multiple objects from unlabeled
-              images is that it is a priori impossible to tell which part of the image corresponds to each...<span
-                class="abs_more">(More)</span></p>
+            <p class="se_jour"><a href="">{{item.Paper_Journal + '(' + item.Journal_Level + ')'}}</a></p>
+            <p class="se_abs" v-show="!(hideAbsDetail ^ pIndex == index)"><span class="se_type">摘要</span>{{item.Paper_Abstract |
+              abstractAbbr}}<span class="abs_more" @click="hideAbsDetailBtn(index)">More</span></p>
+            <p class="se_abs" v-show="showAbsDetail && pIndex == index"><span class="se_type">摘要</span>{{item.Paper_Abstract}}<span class="abs_more"
+                @click="showAbsDetailBtn(index)">Less</span></p>
             <div class="se_relate" title="与我相关">
               <img src="../../assets/images/about.png">
-              <p class="re_keyword"><span>Mobile Network(medium)</span>,<span>Sensing(weak)</span></p>
-              <p class="re_author"><span>Ming Liu(medium)</span></p>
+              <p class="re_keyword" v-for="(Relkeyword,index) in item.Rel_Keyword" :key="index+10000">我的方向：
+                <a> {{Relkeyword.Keyword}}</a>
+                <span> {{Relkeyword.Rel_Level}} </span>,</p>
+              <p class="re_author" v-for="(Relfriend,index) in item.Rel_Friend" :key="index+100000">我的好友：
+                <a>{{Relfriend.Author_Name}}</a>
+                <span>{{Relfriend.Rel_Level}}</span></p>
             </div>
           </div>
         </section>
@@ -46,31 +51,77 @@
       return {
         isShowSearchbox: true,
         bg_mask: false,
-        paperInfo:'',
+        paperInfo: '',
+        showAbsAbbr:true,
+        showAbsDetail: false,
+        hideAbsDetail:false,
+        pIndex:-1,
+        upshowIndex:5,
+        lowshowIndex:0
 
       }
     },
     components: {
       Header
     },
-    created(){
-        this.getPaperInfo()
+    created() {
+      this.getPaperInfo()
     },
     methods: {
-        getPaperInfo(){
-            axios({
-                url:'https://www.easy-mock.com/mock/5b9f5cdbbdb9831993a4272e/readPaper',
-                method:'get'
-            }).then(response=>{
-                if(response.status == 200){
-                    this.paperInfo = response.data
-                }
-            }).catch(error=>{
-                alert('服务器错误，获取数据失败')
-            })
-        },
+      getPaperInfo() {
+        axios({
+          url: 'https://www.easy-mock.com/mock/5b9f5cdbbdb9831993a4272e/readPaper',
+          method: 'get'
+        }).then(response => {
+          if (response.status == 200) {
+            this.paperInfo = response.data
+          }
+        }).catch(error => {
+          alert('服务器错误，获取数据失败')
+        })
+      },
+      chnagePaper(){
+        console.log(this.paperInfo.length)
+        if(this.upshowIndex == this.paperInfo.length){
+          this.lowshowIndex = 0
+          this.upshowIndex = 0
+        }
+        if(this.paperInfo.length < this.upshowIndex + 5){
+          this.lowshowIndex = this.upshowIndex
+          this.upshowIndex = this.paperInfo.length
+        }else{
+          this.lowshowIndex = this.upshowIndex
+          this.upshowIndex += 5
+        }
+      },
+      changepage(index){
+        if(this.lowshowIndex <= index && index < this.upshowIndex){
+          return true
+        }else{
+          return false
+        }
+      },
       isMask(data) {
         this.bg_mask = data
+      },
+      hideAbsDetailBtn(index){
+        this.showAbsDetail = true
+        this.pIndex = index
+      },
+      showAbsDetailBtn(index) {
+        this.showAbsDetail = false
+        this.pIndex = -1
+        console.log(index + 'index')
+      }
+    },
+    filters: {
+      abstractAbbr(abs) {
+        if (abs.length > 420) {
+          let absAbbr = abs.substring(0, 420) + '...'
+          return absAbbr
+        }else{
+          return abs
+        }
       }
     }
   }
@@ -78,8 +129,8 @@
 </script>
 
 <style scoped>
-@import '../../assets/css/viewPaper.css';
-@import '../../assets/css/search.css';
-@import '../../assets/css/base.css';
+  @import '../../assets/css/viewPaper.css';
+  @import '../../assets/css/search.css';
+  @import '../../assets/css/base.css';
 
 </style>
